b2d147dff11dcb74e9b5faab16cdd6095e3e86a1
Fanyas <106335313+Fanyas@users.noreply.github.com>
2022-11-25 14:42:52 +0300

    Add files via upload

---
+++ MyProject/MyProject.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace MyProject.Tests
+{
+    [TestClass]
+    public class MyProjectTests
+    {
+        [TestMethod]
+        public void Logic()
+        {
+            //arrange
+
+            //act
+
+            //assert
+            Assert.IsTrue(true);
+        }
+    }
+}
